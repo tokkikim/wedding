5ab8da320f64c85a5f24cca3f65e2d6ae3326245
tokkikim <165551687+tokkikim@users.noreply.github.com>
2025-10-13 23:33:50 +0900

    chore: install testing dependencies

---
+++ wedding-ai-app/jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  passWithNoTests: true,
+};
+
+module.exports = createJestConfig(customJestConfig);
